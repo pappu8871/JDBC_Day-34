187d45a38caeade007850b1c75b96dac9ae35265
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-19 22:53:05 +0530

    Added:FileIOService

---
+++ EmployeePayrollFileIOService.java
@@ -0,0 +1,20 @@
+package com.practice.jdbc;
+
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+
+	public void writeData(List<EmployeePayrollData> employeePayrollList, Object ioService) {
+		// TODO Auto-generated method stub
+	}
+
+	public void printData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void countEntries() {
+		// TODO Auto-generated method stub
+		
+	}
+}
